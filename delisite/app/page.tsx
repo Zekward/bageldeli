0b101e10454dbee73249668af09e9ca71e2896b3
Zekey <zh437@cornell.edu>
2025-12-26 22:34:14 -0500

    Vercel Error 404 Debug

--- delisite/app/page.tsx
+++ delisite/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <Button size="lg">View Menu</Button>
+      <Button size="lg">View Men</Button>
     </main>
   );
 }
